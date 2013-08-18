483329ae382c00e60e8e19b1a8def437eecf8771
Taylor Fausak <taylor@fausak.me>
2013-08-18 14:16:47 -0500

    Use concat instead of foldl

--- p07.hs
+++ p07.hs
@@ -3,4 +3,4 @@ data NestedList a = Elem a | List [NestedList a]
 flatten :: NestedList a -> [a]
 flatten (Elem x) = [x]
 flatten (List []) = []
-flatten (List xs) = foldl (++) [] $ map flatten xs
+flatten (List xs) = concat $ map flatten xs
