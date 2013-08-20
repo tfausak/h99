6f6dd1768099fc07383d171e54d9a9b1c063a9b8
Taylor Fausak <taylor@fausak.me>
2013-08-19 22:28:28 -0500

    Format code better

--- H99.hs
+++ H99.hs
@@ -13,6 +13,11 @@ pack (x:xs) =
     in if x == head xs then (x : head rest) : tail rest else [x] : rest
 
 -- Modified run-length encoding.
-data Element a = Single a | Multiple Int a deriving (Show)
+data Element a = Single a | Multiple Int a
+    deriving (Show)
+
 encodeModified :: (Eq a) => [a] => [Element a]
-encodeModified xs = [if length x == 1 then Single (head x) else Multiple (length x) (head x) | x <- pack xs]
+encodeModified xs = [
+    if length x == 1
+    then Single (head x)
+    else Multiple (length x) (head x) | x <- pack xs]
