ba76162a1dfae32575b4e497277cdae2941e23e6
Taylor Fausak <taylor@fausak.me>
2013-08-19 22:24:47 -0500

    Extract common code

--- H99.hs
+++ H99.hs
@@ -1,5 +1,7 @@
 module H99
-( pack
+( Element(..)
+, encodeModified
+, pack
 ) where
 
 -- Pack consecutive duplicates of list elements into sublists.
@@ -9,3 +11,8 @@ pack [x] = [[x]]
 pack (x:xs) =
     let rest = pack xs
     in if x == head xs then (x : head rest) : tail rest else [x] : rest
+
+-- Modified run-length encoding.
+data Element a = Single a | Multiple Int a deriving (Show)
+encodeModified :: (Eq a) => [a] => [Element a]
+encodeModified xs = [if length x == 1 then Single (head x) else Multiple (length x) (head x) | x <- pack xs]
--- p11.hs
+++ p11.hs
@@ -1,6 +1,1 @@
-import H99 (pack)
-
--- Modified run-length encoding.
-data Element a = Single a | Multiple Int a deriving (Show)
-encodeModified :: (Eq a) => [a] => [Element a]
-encodeModified xs = [if length x == 1 then Single (head x) else Multiple (length x) (head x) | x <- pack xs]
+import H99 (encodeModified)
