34e3c97c8af7c8b5f55ca09f8768140a103c8d12
Taylor Fausak <taylor@fausak.me>
2013-09-05 21:57:49 -0500

    Speed up the primality checker

--- p31.hs
+++ p31.hs
@@ -1,5 +1,12 @@
 -- Determine whether a given integer number is prime.
 isPrime :: Int -> Bool
-isPrime n =
-  let bound n = [1 .. floor . sqrt $ fromIntegral n]
-  in [d | d <- bound n, rem n d == 0] == [1]
+isPrime n
+    | n < 2          = False -- Integers less than 2 are not prime.
+    | n < 4          = True  -- 2 and 3 are prime.
+    | even n         = False -- Even integers are composite.
+    | n < 9          = True  -- 5 and 7 are prime.
+    | n `rem` 3 == 0 = False -- Multiples of 3 are composite.
+    -- All remaining primes have the form 6k±1.
+    | otherwise = not $ any (isComposite n) [5, 11 .. limit n] where
+        isComposite n d = n `rem` d == 0 || n `rem` (d + 2) == 0
+        limit = floor . sqrt . fromIntegral
